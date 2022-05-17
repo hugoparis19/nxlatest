f6dcf490994eaf9879d7d92096735e57b160c768
hugo <hugo.paris19@gmail.com>
2022-05-17 13:53:35 +0200

    initial commit

---
+++ apps/first-code-space/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxlatest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'first-code-space';
+}
